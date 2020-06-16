531076b9317840c4ce57c8f915b3640a8c3bdf4a
Debonex <719893090@qq.com>
2020-06-16 17:06:32 +0800

    +jquery element-ui bootstrap dependency

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="card">
       用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
       <br><br>
       密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import $ from 'jquery'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 Vue.config.productionTip = false
 
 // reverse Proxy
@@ -13,6 +17,7 @@ axios.defaults.baseURL = 'http://localhost:8081/api'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
