82780d6324a530dc879ab7593248ece6dc567e4e
Debonex <719893090@qq.com>
2020-06-18 09:04:17 +0800

    +register front ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="row container-login">
     <div class="card col-3 container-form">
-      <h5><b>登入智能家居系统</b></h5>    
-      <input type="text" class="form-control" v-model="loginForm.username" placeholder="请输入用户名">
-      <input type="password" class="form-control" v-model="loginForm.password" placeholder="请输入密码">
-      <button v-on:click="login" class="btn btn-success">登录</button>
+      <div class="container-form-header">
+        <h5><b>{{isLoginWindow?'登录':'注册'}}智能家居系统</b></h5>
+        <a href="#" @click="isLoginWindow=!isLoginWindow" style="position:absolute;right:2rem">{{isLoginWindow?'注册':'登陆'}}</a>
+      </div>
+      <div class="alert alert-danger" v-if="isAlert">{{alertMessage}}</div>
+      <template v-if="isLoginWindow">
+        <input type="text" class="form-control" v-model="loginForm.username" placeholder="请输入用户名">
+        <input type="password" class="form-control" v-model="loginForm.password" placeholder="请输入密码">
+        <button  v-on:click="login" class="btn btn-success">登录</button>
+      </template>
+      <template v-else>
+        <input type="text" class="form-control" v-model="registerForm.username" placeholder="请输入用户名">
+        <input type="password" class="form-control" v-model="registerForm.password" placeholder="请输入密码">
+        <button  v-on:click="register" class="btn btn-success">注册</button>
+      </template>
     </div>
     <wave class="animation-wave"></wave>
   </div>
 </template>
 
 <script>
-  import wave from './animations/Wave'
+  import wave from './animations/Wave'  
 
   export default {
     name: 'Login',
     data () {
       return {
+        isLoginWindow: true,
+        isAlert:false,
+        alertMessage:'',
         loginForm: {
           username: '',
           password: ''
+        },
+        registerForm:{
+          username:'',
+          password:''
         },
         responseResult: []
       }
@@ -34,11 +52,27 @@
           .then(successResponse => {
             if (successResponse.data.code === 200) {
               this.$router.replace({path: '/index'})
+            }else{
+              this.isAlert=true
+              alertMessage='登录失败'
             }
           })
           .catch(failResponse => {
             console.error(failResponse)
           })
+      },
+      regitster(){
+        this.$axios.post('/register',{
+          username: this.registerForm.username,
+          password: this.registerForm.password
+        }).then(succ=>{
+          if(succ.data.code===200){
+            
+          }else{
+            this.isAlert=true
+            alertMessage='登录失败'
+          }
+        })
       }
     },components:{
       wave
@@ -58,6 +92,11 @@
     width: 15rem;
     height: 25rem;
     justify-content: space-around;
+  }
+
+  .container-form-header{
+    display: flex;
+    justify-content: center;
   }
 
   .animation-wave{
--- src/components/home/Device.vue
+++ src/components/home/Device.vue
@@ -13,6 +13,7 @@
 
 <script>
 export default {
+  name: 'Device',
   props: {
     id: {
       required: true
